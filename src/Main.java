daa77dd76527f86ab528b37e1c3bfad30f51c3a8
RafaelMaciasT <maciasjesus03@gmail.com>
2025-04-22 18:36:18 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -16,11 +16,69 @@ public class Main {
 		switch (opciones) {
 		case 0:
 			
+			JOptionPane.showMessageDialog(null, "Seleccion de inicio de cuenta");
+			
+			String []Accmenu= {"Empleado" , "Cliente","Salir"};
+			int accop;
+			do {
+				accop=JOptionPane.showOptionDialog(null, "Selecciones el Tipo de Usuario", "Seleccion de Inicio", 0, 0, null, Accmenu, Accmenu[0]);
+				
+				switch (accop) {
+				case 0:
+					JOptionPane.showMessageDialog(null, "Seleccion de inicio de cuenta");
+					
+					String []empmenu= {"Empleado" , "Cliente","Salir"};
+					int empop;
+					do {
+						empop=JOptionPane.showOptionDialog(null, "Selecciones el Tipo de Usuario", "Seleccion de Inicio", 0, 0, null, empmenu, empmenu[0]);
+						
+						switch (empop) {
+						case 0:
+							
+							break;
+
+						case 1:
+							break;
+						case 2: 
+						default:
+							break;
+						}
+					} while (empop!=2);
+					break;
+				
+
+				case 1:
+					break;
+				case 2: 
+				default:
+					break;
+				}
+			} while (accop!=2);
 			break;
 
 		case 1:
+            JOptionPane.showMessageDialog(null, "Registro de Cuenta");
 			
+			String []regmenu= {"Empleado" , "Cliente","Salir"};
+			int regop;
+			do {
+				regop=JOptionPane.showOptionDialog(null, "Tipo de Usuario para Registrar", "Seleccion de Registro", 0, 0, null, regmenu, regmenu[0]);
+				
+				switch (regop) {
+				case 0:
+					
+					break;
+
+				case 1:
+					break;
+				case 2: 
+				default:
+					break;
+				}
+			} while (regop!=2);
 			break;
+			
+			
 		case 2:
 			
 		default:
