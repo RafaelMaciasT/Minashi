fcc3bfc533e943240219deb4d0f4977639e6cd2d
RafaelMaciasT <maciasjesus03@gmail.com>
2025-04-23 19:48:28 -0300

    Create Empleado.java

---
+++ src/Empleado.java
@@ -0,0 +1,29 @@
+
+public class Empleado extends Persona{
+
+	private int nro_empleado;
+	private int pin;
+	public Empleado(String nombre, String apellido, String mail, int nro_empleado, int pin) {
+		super(nombre, apellido, mail);
+		this.nro_empleado = nro_empleado;
+		this.pin = pin;
+	}
+	public int getNro_empleado() {
+		return nro_empleado;
+	}
+	public void setNro_empleado(int nro_empleado) {
+		this.nro_empleado = nro_empleado;
+	}
+	public int getPin() {
+		return pin;
+	}
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+	@Override
+	public String toString() {
+		return "Empleado [nro_empleado=" + nro_empleado + ", pin=" + pin + "]";
+	}
+	
+	
+}
